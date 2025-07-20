c226b5585cdfe791df80b9b654666f4a31fabe9a
Ashwini Kumar <shahi.ashwini29@gmail.com>
2025-07-20 18:28:36 +0530

    tailwind, react-router and

---
+++ frontend/frontend/src/Auth/Signup.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <div className="hero bg-base-500 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <fieldset className="fieldset">
+                <label className="label">Name</label>
+                <input type="text" className="input" placeholder="Name" />
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <label className="label">Phone</label>
+                <input type="number" className="input" placeholder="Phone" />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
