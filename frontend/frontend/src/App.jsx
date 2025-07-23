fe46e0cefdbaf46fd11c1114e77d661605b06d71
Ashwini Kumar <shahi.ashwini29@gmail.com>
2025-07-24 00:08:16 +0530

    route protection added

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -8,11 +8,13 @@ import Login from "./Auth/Login";
 import NotFound from "./pages/NotFound";
 import Signup from "./Auth/Signup";
 import Home from "./components/Home";
+import Redirect from "./pages/Redirect";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/login" />,
+    element: <Redirect />,
   },
   {
     path: "/login",
@@ -26,7 +28,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/home",
-    element: <Home />,
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
     errorElement: <NotFound />,
   },
   {
